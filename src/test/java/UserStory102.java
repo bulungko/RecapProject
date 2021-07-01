d1e13be101b6ad7af298c6173ae85207cfb831d3
bulungko <79658245+bulungko@users.noreply.github.com>
2021-06-30 21:28:04 -0400

    Us102 has been completed (#2)
    us102 has squashed 3 commited and merged

---
+++ src/test/java/UserStory102.java
@@ -0,0 +1,10 @@
+public class UserStory102 {
+    //lots of code has been written here
+    //ready to push this branch to remote
+    //and open pull request so i cn start collaborating
+    //so my team member can comment, review and approve or request for more changes
+    public static void main(String[] args) {
+        //more changes added as requested
+
+    }
+}
