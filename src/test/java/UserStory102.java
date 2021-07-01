098d6d60f4ed85c6aa729a96e2fd6c6f2c126609
bulungko <bulungko@yahoo.com>
2021-06-30 21:18:59 -0400

    user story102 work1

--- src/test/java/UserStory102.java
+++ src/test/java/UserStory102.java
@@ -3,4 +3,7 @@ public class UserStory102 {
     //ready to push this branch to remote
     //and open pull request so i cn start collaborating
     //so my team member can comment, review and approve or request for more changes
+    public static void main(String[] args) {
+
+    }
 }
